c2d63cb1c565bd96c8325d730e830e81f0bc7642
Hang Phung <hangpt@hangpt-Ubuntu20.04>
2022-04-06 10:46:31 +0700

    Lab01 - main

---
+++ PHP_Test/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            
+        </title>
+    </head>
+    <body>
+        <?php
+//            echo "Hello, world! This is my first PHP project!";
+            phpinfo();
+        ?>
+    </body>
+</html>
+
+
