cc609c83e444cc0634b97a49cedf45be2fcddc65
Hang Phung <hangpt@hangpt-Ubuntu20.04>
2022-04-20 10:50:30 +0700

    Fix Lab03

--- Lab03/Exercise/Lab03-2/DateTimeFunction.php
+++ Lab03/Exercise/Lab03-2/DateTimeFunction.php
@@ -7,23 +7,19 @@
             <table>
                 <tr>
                     <td> First Person's Name: </td>
-                    <td colspan="3"> <input type="text" name="name1"> </td>
+                    <td> <input type="text" name="name1"> </td>
                 </tr>
                 <tr>
-                    <td> First Person's Birthday: (in dd/mm/yyyy) </td>
-                    <td> <input type="number" min="1" max="31" name="day1"> / </td>
-                    <td> <input type="number" min="1" max="12" name="month1"> / </td>
-                    <td> <input type="number" min="1022" max="2022" name="year1"> </td>
+                    <td> First Person's Birthday: (in mm/dd/yyyy) </td>
+                    <td> <input type="text" name="day1"> </td>
                 </tr>
                 <tr>
                     <td> Second Person's Name: </td>
-                    <td colspan="3"> <input type="text" name="name2"> </td>
+                    <td> <input type="text" name="name2"> </td>
                 </tr>
                 <tr>
-                    <td> Second Person's Birthday: (in dd/mm/yyyy) </td>
-                    <td> <input type="number" min="1" max="31" name="day2"> / </td>
-                    <td> <input type="number" min="1" max="12" name="month2"> / </td>
-                    <td> <input type="number" min="1022" max="2022" name="year2"> </td>
+                    <td> Second Person's Birthday: (in mm/dd/yyyy) </td>
+                    <td> <input type="text" name="day2"> </td>
                 </tr>
                 <tr>
                     <td align="right"> <input type="submit" value="Submit"> </td>
@@ -31,40 +27,44 @@
                 </tr>
             </table>
         </form>
-        
+
         <font size="4" color="blue"> Date Time Information </font>
         <?php
 
-        function verify($day, $month, $year) {
-            if () {
-                if ($type == 1) {
-                    $value2 = $value1 * 180 / pi();
-                    return 1;
-                } elseif ($type == 2) {
-                    $value2 = $value1 * pi() / 180;
-                    return 1;
-                } else {
-                    print ("Unknown convert type = $type");
-                    return 0;
-                }
-            } else {
-                return 0;
-            }
+        function validateDate($date, $format = 'd/m/Y') {
+            $d = DateTime::createFromFormat($format, $date);
+            return $d && $d->format($format) === $date;
         }
 
-        $value1 = $_POST["value1"];
-        $type = $_POST["type"];
-        $value2 = 0;
-        $ret = convert($type, $value1, $value2);
-        if (array_key_exists("value1", $_POST)) {
-            if ($ret) {
-                if ($type == 1) {
-                    printf("<br>Converted %.4f radians to %.4f degrees.", $value1, $value2);
-                } else {
-                    printf("<br>Converted %.4f degrees to %.4f radians.", $value1, $value2);
-                }
+        $name1 = $_GET["name1"];
+        $day1 = $_GET["day1"];
+        $name2 = $_GET["name1"];
+        $day2 = $_GET["day2"];
+        if (array_key_exists("name1", $_GET)) {
+            if (validateDate($day1) && validateDate($day2)) {
+                print ("<br><br>These are two valid birthdays.<br>");
+
+                $date1 = date_create($day1);
+                $date2 = date_create($day2);
+                print("<br>The first birthday is: ");
+                echo date_format($date1, "l, F j, Y");
+                print("<br>The second birthday is: ");
+                echo date_format($date2, "l, F j, Y");
+
+                $diff = date_diff($date1, $date2);
+                printf("<br><br>The difference in days between two dates is: ");
+                echo $diff->format("%a days.");
+
+                $age1 = date_diff($date1, date_create($today));
+                print ("<br><br>The first person's age is: ");
+                echo $age1->format("%y.");
+                $age2 = date_diff($date2, date_create($today));
+                print ("<br>The second person's age is: ");
+                echo $age2->format("%y.");
+                print("<br>The different years between two people is: ");
+                echo $diff->format("%y.");
             } else {
-                print"<br>Illegal value received";
+                print ("<br><br>Exists at least one invalid birthdays.<br>");
             }
         }
         ?>
